b227fc4cb9a6a0cf5517cd6f7a4aaed0962d2b6a
Get-Bad <getbad@zohomail.eu>
2023-09-04 21:36:08 +0300

    otmena otmeni

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <!-- -->
+<!-- -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -38,7 +38,7 @@
             if ($conn == false) {  
               echo "Ошибка подключения базы данных";  
             } else {                                
-              echo "База данных подключена";                  
+              echo "Подключено";                  
             }
         */    
     ?>
